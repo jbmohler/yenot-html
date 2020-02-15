dee747123be76de8c02a3193d7605815093ae097
Joel B. Mohler <joel@kiwistrawberry.us>
2020-02-15 03:58:43 -0500

    contact url state; mobile formatting

--- yenothtml/application.py
+++ yenothtml/application.py
@@ -21,20 +21,34 @@ def static():
     global STATIC_ROOT
     return static_file('loginpin.html', root=STATIC_ROOT)
 
-@app.get('/application.html', name='application_html', skip=['yenot-auth'])
-def static():
+@app.get('/contacts/<tail:path>', name='contacts_html', skip=['yenot-auth'])
+def static(tail):
     global STATIC_ROOT
-    return static_file('application.html', root=STATIC_ROOT)
+    response = static_file('contacts.html', root=STATIC_ROOT)
+    response.set_cookie('pathtail', tail)
+    return response
 
 @app.get('/contacts.html', name='contacts_html', skip=['yenot-auth'])
 def static():
     global STATIC_ROOT
     return static_file('contacts.html', root=STATIC_ROOT)
 
+@app.get('/finances/<tail:path>', name='finances_html', skip=['yenot-auth'])
+def static(tail):
+    global STATIC_ROOT
+    response = static_file('finances.html', root=STATIC_ROOT)
+    response.set_cookie('pathtail', tail)
+    return response
+
 @app.get('/finances.html', name='finances_html', skip=['yenot-auth'])
 def static():
     global STATIC_ROOT
     return static_file('finances.html', root=STATIC_ROOT)
+
+@app.get('/application.html', name='application_html', skip=['yenot-auth'])
+def static():
+    global STATIC_ROOT
+    return static_file('application.html', root=STATIC_ROOT)
 
 @app.route('/static/<path:path>', name='static', skip=['yenot-auth'])
 def static(path):

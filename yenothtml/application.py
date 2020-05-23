5116acc4960c9a4651390cdd69bde7abeeb20cb2
Joel B. Mohler <joel@kiwistrawberry.us>
2020-05-22 21:47:21 -0400

    fix names to be unique

--- yenothtml/application.py
+++ yenothtml/application.py
@@ -26,7 +26,7 @@ def static():
     global STATIC_ROOT
     return static_file('lms-avatar.png', root=STATIC_ROOT)
 
-@app.get('/contacts/<tail:path>', name='contacts_html', skip=['yenot-auth'])
+@app.get('/contacts/<tail:path>', name='contacts_doc_html', skip=['yenot-auth'])
 def static(tail):
     global STATIC_ROOT
     response = static_file('contacts.html', root=STATIC_ROOT)
@@ -38,7 +38,7 @@ def static():
     global STATIC_ROOT
     return static_file('contacts.html', root=STATIC_ROOT)
 
-@app.get('/finances/<tail:path>', name='finances_html', skip=['yenot-auth'])
+@app.get('/finances/<tail:path>', name='finances_doc_html', skip=['yenot-auth'])
 def static(tail):
     global STATIC_ROOT
     response = static_file('finances.html', root=STATIC_ROOT)

bc5111edd8a164b99b5e6dcfce05711e17aea062
Joel B. Mohler <joel@kiwistrawberry.us>
2021-05-28 22:41:45 -0400

    send down rtx_prefix cookie for api access

--- yenothtml/application.py
+++ yenothtml/application.py
@@ -1,5 +1,5 @@
 import os
-from bottle import static_file
+from bottle import static_file, request
 import yenot.backend.api as api
 
 app = api.get_global_app()
@@ -9,12 +9,16 @@ STATIC_ROOT = os.path.join(os.path.dirname(os.path.normpath(__file__)), 'static'
 @app.get('/', name='root_html', skip=['yenot-auth'])
 def static():
     global STATIC_ROOT
-    return static_file('root.html', root=STATIC_ROOT)
+    response = static_file('root.html', root=STATIC_ROOT)
+    response.set_cookie('rtx_prefix', request.environ['YENOT_BASE_URL'])
+    return response
 
 @app.get('/login.html', name='login_html', skip=['yenot-auth'])
 def static():
     global STATIC_ROOT
-    return static_file('login.html', root=STATIC_ROOT)
+    response = static_file('login.html', root=STATIC_ROOT)
+    response.set_cookie('rtx_prefix', request.environ['YENOT_BASE_URL'])
+    return response
 
 @app.get('/loginpin.html', name='loginpin_html', skip=['yenot-auth'])
 def static():
@@ -31,29 +35,37 @@ def static(tail):
     global STATIC_ROOT
     response = static_file('contacts.html', root=STATIC_ROOT)
     response.set_cookie('pathtail', tail)
+    response.set_cookie('rtx_prefix', request.environ['YENOT_BASE_URL'])
     return response
 
 @app.get('/contacts.html', name='contacts_html', skip=['yenot-auth'])
 def static():
     global STATIC_ROOT
-    return static_file('contacts.html', root=STATIC_ROOT)
+    response = static_file('contacts.html', root=STATIC_ROOT)
+    response.set_cookie('rtx_prefix', request.environ['YENOT_BASE_URL'])
+    return response
 
 @app.get('/finances/<tail:path>', name='finances_doc_html', skip=['yenot-auth'])
 def static(tail):
     global STATIC_ROOT
     response = static_file('finances.html', root=STATIC_ROOT)
     response.set_cookie('pathtail', tail)
+    response.set_cookie('rtx_prefix', request.environ['YENOT_BASE_URL'])
     return response
 
 @app.get('/finances.html', name='finances_html', skip=['yenot-auth'])
 def static():
     global STATIC_ROOT
-    return static_file('finances.html', root=STATIC_ROOT)
+    response = static_file('finances.html', root=STATIC_ROOT)
+    response.set_cookie('rtx_prefix', request.environ['YENOT_BASE_URL'])
+    return response
 
 @app.get('/reports.html', name='reports_html', skip=['yenot-auth'])
 def static():
     global STATIC_ROOT
-    return static_file('reports.html', root=STATIC_ROOT)
+    response = static_file('reports.html', root=STATIC_ROOT)
+    response.set_cookie('rtx_prefix', request.environ['YENOT_BASE_URL'])
+    return response
 
 @app.route('/static/<path:path>', name='static', skip=['yenot-auth'])
 def static(path):
@@ -63,14 +75,20 @@ def static(path):
 @app.get("/lms/user-profile", name="get_lms_user_profile", skip=['yenot-auth'])
 def get_lms_user_profile():
     global STATIC_ROOT
-    return static_file("profile.html", root=STATIC_ROOT)
+    response = static_file("profile.html", root=STATIC_ROOT)
+    response.set_cookie('rtx_prefix', request.environ['YENOT_BASE_URL'])
+    return response
 
 @app.get("/lms/technical", name="get_lms_technical", skip=["yenot-auth"])
 def get_lms_technical():
     global STATIC_ROOT
-    return static_file("technical.html", root=STATIC_ROOT)
+    response = static_file("technical.html", root=STATIC_ROOT)
+    response.set_cookie('rtx_prefix', request.environ['YENOT_BASE_URL'])
+    return response
 
 @app.get("/lms/diagnostics", name="get_lms_diagnostics", skip=["yenot-auth"])
 def get_lms_diagnostics():
     global STATIC_ROOT
-    return static_file("diagnostics.html", root=STATIC_ROOT)
+    response = static_file("diagnostics.html", root=STATIC_ROOT)
+    response.set_cookie('rtx_prefix', request.environ['YENOT_BASE_URL'])
+    return response
